2b69a15fe5c9726afc0e8a176e642797c881f2f3
Inna Lapteva <innalapteva72@gmail.com>
2020-05-21 15:30:31 +0500

    Initial Commit

---
+++ Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipes
+//
+//  Created by Инна Лаптева on 21.05.2020.
+//  Copyright © 2020 Инна Лаптева. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
